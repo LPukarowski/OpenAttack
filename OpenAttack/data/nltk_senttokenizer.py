05ce32a685475c84b4b8fb330711855598962192
Logan Pukarowski <130484804+LPukarowski@users.noreply.github.com>
2026-06-04 18:26:48 -0400

    Update nltk_senttokenizer.py

--- OpenAttack/data/nltk_senttokenizer.py
+++ OpenAttack/data/nltk_senttokenizer.py
@@ -1,18 +1,16 @@
 """
 :type: function
 :Size: 158.351KB
-
 Model files for nltk punkt sentence tokenizer.
 """
 from OpenAttack.utils import make_zip_downloader
 import os
+import pickle
 
 NAME = "TProcess.NLTKSentTokenizer"
-
 URL = "/TAADToolbox/punkt.english.pickle.zip"
 DOWNLOAD = make_zip_downloader(URL, "english.pickle")
 
-
 def LOAD(path):
-    return __import__("nltk").data.load("file:" + os.path.join(path, "english.pickle")).tokenize
-
+    with open(os.path.join(path, "english.pickle"), "rb") as f:
+        return pickle.load(f).tokenize
